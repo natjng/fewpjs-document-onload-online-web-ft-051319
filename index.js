cd2a3ab9932b37fcfac01a505426b41d6ec10803
Natalie <natsetc@gmail.com>
2019-08-23 00:46:27 -0700

    notes

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelector("#text").textContent = "This is really cool!"
 });
+
+// document.addEventListener( "DOMContentLoaded", function () {
+//     updateDOM()
+// } );
+  
+// function updateDOM() {
+//     document.getElementById("text").innerHTML = "This is really cool!";
+// }
